9c1f7c2ce22e5ac9214187c09d733b612621679f
James R. Barlow <james@purplerock.ca>
2021-08-13 16:25:42 -0700

    Powershell again

--- build-scripts/win-download-qpdf.ps1
+++ build-scripts/win-download-qpdf.ps1
@@ -1,6 +1,5 @@
-$platform = $args[0]
-
-$version = $Env:QPDF_VERSION
+$version = $args[0]
+$platform = $args[1]
 
 if ($platform -eq "win_amd64") {
     $msvc = "msvc64"
@@ -13,7 +12,6 @@ if ($platform -eq "win_amd64") {
 if (($version -eq "10.3.2") -and ($msvc -eq "msvc32")) {
     $msvc = "msvc32-rebuild"
 }
-
 
 $qpdfurl = "https://github.com/qpdf/qpdf/releases/download/release-qpdf-$version/qpdf-$version-bin-$msvc.zip"
 echo "Download $qpdfurl"
